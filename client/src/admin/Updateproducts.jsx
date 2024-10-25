942b908981b859ea1aade84921d8c32c05bdc4fc
Natsudraganeel <149077586+Natsudraganeel@users.noreply.github.com>
2024-10-25 18:42:25 +0530

    Update Updateproducts.jsx

--- client/src/admin/Updateproducts.jsx
+++ client/src/admin/Updateproducts.jsx
@@ -150,7 +150,7 @@ return (
                 <div className="my-3">
                 <label type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">  
                 {photo ? photo.name :"Upload Image"}
-                <input  style={{display:"none"}} accept="images/*" type="file" onChange={(e)=>setphoto(e.target.files[0])}/>
+                <input  style={{display:"none"}} accept="image/*" type="file" onChange={(e)=>setphoto(e.target.files[0])}/>
                 </label>
               
                 </div>
