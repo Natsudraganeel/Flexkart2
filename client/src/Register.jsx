e0b651e7706ddd4882a3c5520e10c29fa24cf59a
Natsudraganeel <149077586+Natsudraganeel@users.noreply.github.com>
2025-06-25 22:58:15 +0530

    Update Register.jsx

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -105,7 +105,7 @@ export default function Register(){
             <input onChange={change1} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
           </div>
           <div>
-            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">Confirm Password</label>
+            <label htmlFor="cpassword" className="block mb-2 text-sm font-medium text-gray-900 ">Confirm Password</label>
             <input onChange={change2} type="password" name="cpassword" id="cpassword" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
           </div>
           <h1 style={{color:"red"}}>{click}</h1>
