c6137664f55abae476c5caba93fd4907f5437916
Natsudraganeel <149077586+Natsudraganeel@users.noreply.github.com>
2025-06-29 19:43:38 +0530

    Update authroute.js(entered emailcheck)

--- server/routes/authroute.js
+++ server/routes/authroute.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { requiredsignin,isAdmin } from "../middleware/authmiddleware.js";
-import { orderstatuscontroller,allordersadmincontroller,logincontroller, registercontroller,forgotcontroller,updateprofilecontroller,allorderscontroller } from "../controllers/authcontroller.js";
+import { orderstatuscontroller,allordersadmincontroller,emailcheck,logincontroller, registercontroller,forgotcontroller,updateprofilecontroller,allorderscontroller } from "../controllers/authcontroller.js";
 const router=express.Router();
 router.post("/register",registercontroller)
 router.post("/login",logincontroller);
 // router.get("/test",requiredsignin,test);
 router.post("/forgotpassword",forgotcontroller);
+router.post("/emailcheck",emailcheck);
 router.put("/updateprofile",requiredsignin,updateprofilecontroller);
 router.get("/allorders/:id",requiredsignin,allorderscontroller);
 router.get("/orders",requiredsignin,isAdmin,allordersadmincontroller);
